9c67300ef982300c1415bf454c9b1adcd4b2e268
Josh Sisto <joshsisto@gmail.com>
2017-06-07 18:20:05 -0700

    Update 48Tweets.py

--- 48Tweets/48Tweets.py
+++ 48Tweets/48Tweets.py
@@ -26,18 +26,33 @@ from FortyEightLawsDictionary import *
 
 
 
-# print(laws_list)
-# print(laws_list[0:47])
-
 l = laws_list
 
+## https://stackoverflow.com/questions/18854620/whats-the-best-way-to-split-a-string-into-fixed-length-chunks-and-work-with-the
+## create a definition to chunk strings into a specified size. Going to use to stay under Twitters character limit
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
 
+## Creates a numpy array and then creates a variable with the definition of Law 1
+np_l = np.array(l)
+np_l_one = (np_l[0, 2])
+## Creates a variable and chunks the definition of Law 1 into 120 Character chunks
+chunk_l = list(chunkstring(np_l_one, 120))
+print(chunk_l)
 
+## This prints Law 1 Never Outshine the Master and then prints the Law
+np_l = np.array(l)
+print(np_l[0, 0] + ' ' + np_l[0, 1])
+print(np_l[0, 2])
 
-# ## This prints Law 1
-# np_l = np.array(l)
-# print(np_l[0,0])
+## This prints all of the laws (the first item of each list)
+np_l = np.array(l)
+print(np_l[:, 0])
 
-# ## This will print each law out
-# for s in l:
-#     print(*s)
+## This prints Law 1
+np_l = np.array(l)
+print(np_l[0, 0])
+
+## This will print each law out
+for s in l:
+    print(*s)
